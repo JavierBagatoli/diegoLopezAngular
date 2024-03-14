580bd9d8e21f11f3042d6e212505576851cbd84f
Javier Bagatoli <javi.bagatoli@gmail.com>
2024-03-14 20:22:46 -0300

    navbar funcional al 100 %

--- src/shards/navbar/navbar.component.ts
+++ src/shards/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,27 +10,14 @@ export class NavbarComponent implements OnInit {
   items: MenuItem[] = [];
   activeItem: MenuItem | undefined;
   showDatos: boolean = false
+  @Output() pageTwo = new EventEmitter<boolean>();
 
   ngOnInit() {
-      this.items = [{
-          label: 'Work',
-          icon: '', 
-          items: [
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-            },
-            {
-                label: 'New',
-                icon: 'pi pi-fw pi-plus',
-            }
-          ]
-        
-        
-          },
-          { label: 'Portfolio', icon: '', items: []},
-          { label: 'Store', icon: '', items: []},
-          { label: 'About Me', icon: '', items: [] }
+      this.items = [{label: 'Work',icon: ''},
+          { label: 'Portfolio', icon: ''},
+          { label: 'To Behance', icon: ''},
+          { label: 'To Artstation', icon: ''},
+          { label: 'About Me', icon: ''}
       ];
 
       this.activeItem = this.items[0];
@@ -38,19 +25,31 @@ export class NavbarComponent implements OnInit {
 
   changes(argument: any): void {
     console.log(argument)
+      if(argument.label === this.items[1].label){
+        this.pageTwo.emit(true)
+      }else{
+        this.pageTwo.emit(false)
+      }
+
+      if(argument.label === this.items[2].label){
+        window.open("https://www.behance.net/diegolopezart", "_blank");
+      }
+      
       if(argument.label === this.items[3].label){
+        window.open("https://www.artstation.com/diegolopez_artist", "_blank");
+      }
+      
+      
+
+      if(argument.label === this.items[4].label){
         this.showDatos = true
       }else{
         this.showDatos = false
       }
-      
     }
 }
-
-  
 
 interface  MenuItem{
   label: string,
   icon: string,
-  items: any[]
 }
