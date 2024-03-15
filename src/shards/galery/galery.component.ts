b61eace2e5d7e61d271a7156f65b19d0bd89e53b
Javier Bagatoli <javi.bagatoli@gmail.com>
2024-03-14 22:55:44 -0300

    galeria

--- src/shards/galery/galery.component.ts
+++ src/shards/galery/galery.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-galery',
@@ -7,4 +7,90 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './galery.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GaleryComponent { }
+export class GaleryComponent implements OnInit {
+  @Output() imageSelected = new EventEmitter<any>();
+
+  vector = [
+    {url: "../assets/Trabajo/Clothes.jpg",
+     name: "Rysn and Chiri-Chiri"}
+    ,
+    {url: "../assets/Trabajo/Clothes.jpg",
+     name: ""}
+    ,
+    {url: "../assets/Trabajo/Dawn.jpg",
+      name: ""}
+     ,
+     {url: "../assets/Trabajo/Diagrama.jpg",
+      name: ""}
+     ,
+     {url: "../assets/Trabajo/Frodo.jpg",
+      name: ""}
+     ,
+     {url: "../assets/Trabajo/Hoid.jpg",
+      name: ""}
+     ,
+     {url: "../assets/Trabajo/HoidSpirits.jpg",
+      name: ""}
+     ,
+     {url: "../assets/Trabajo/Storm.jpg",
+      name: "",
+      size: "horizontal"
+    }
+     ,
+     {url: "../assets/Trabajo/Tress.jpg",
+      name: ""}
+    ,
+    {url: "../assets/Trabajo/Yumi.jpg",
+      name: ""}
+    ,
+    {url: "../assets/Trabajo/Clothes.jpg",
+      name: ""}
+     ,
+     {url: "../assets/Trabajo/Yumi.jpg",
+      name: ""}
+    ,
+    {url: "../assets/Trabajo/Yumi.jpg",
+      name: ""},
+      {url: "../assets/Trabajo/Tress.jpg",
+       name: ""}
+     ,
+     {url: "../assets/Trabajo/Yumi.jpg",
+       name: ""}
+     ,
+     {url: "../assets/Trabajo/Clothes.jpg",
+       name: ""}
+      ,
+      {url: "../assets/Trabajo/Yumi.jpg",
+       name: ""}
+     ,
+     {url: "../assets/Trabajo/Yumi.jpg",
+       name: ""}
+
+    
+    
+    
+    
+    
+  ]
+
+  ngOnInit(): void {
+    const img = new Image();
+    this.vector.forEach(
+      image => {
+        img.src = image.url;
+        console.log(image.url, img.naturalWidth, img.naturalHeight)
+      }
+    )
+  }
+
+  selectImage(element : any){
+    let dato : string = element.url
+
+    let dato2 = dato.slice(6)
+    
+    this.imageSelected.emit(
+        {url: dato2,
+         name: element.name
+        })
+}
+ }
